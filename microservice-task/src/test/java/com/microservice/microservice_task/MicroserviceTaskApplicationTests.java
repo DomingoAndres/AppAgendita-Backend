daac0e65503f2c2d53e01160c78c481006cc9022
Areliz Isla <are.isla@duocuc.cl>
2025-11-16 13:44:49 -0300

    se sube microservicio de task, pero faltan cosas todavía

---
+++ microservice-task/src/test/java/com/microservice/microservice_task/MicroserviceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.microservice_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
